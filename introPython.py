cd014a598853f3bbc7f58b067b48185b6dfb83f3
surbhirajpal <39900565+surbhirajpal@users.noreply.github.com>
2018-06-13 15:42:15 +0530

    Add files via upload

---
+++ introPython.py
@@ -0,0 +1,28 @@
+#Q1
+print("hello world")
+
+#Q2
+print("acad"+"view")
+
+#Q3
+x=10
+y=20
+z=30
+print (x)
+print(y)
+print(z)
+
+#Q4
+print("let\'s get started")
+
+#Q5
+s="acadview"
+course="python"
+fees=5000
+print("hi,welcome to %s your course is %s and your fees is %d"%(s,course,fees))
+
+
+#Q6
+name="tony stark "
+salary=1000000
+print(('%s''%d')%(name,salary))

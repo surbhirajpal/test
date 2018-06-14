2b29ef9c1982f9705ec0bf45457126ec62c30fb9
surbhirajpal <39900565+surbhirajpal@users.noreply.github.com>
2018-06-14 21:47:55 +0530

    Update modules.py

--- modules.py
+++ modules.py
@@ -1,6 +1,6 @@
 #Q1
 '''
-IME TUPLE-
+TIME TUPLE-
 We represent time in a way that’s easy for us to understand. However, Python stores it in tuples.
 These python tuples are made of nine numbers.
 Index	 Field	            Domain of Values
@@ -9,9 +9,9 @@ Index	 Field	            Domain of Values
 2	     Day	            1 to 31
 3	     Hour	            0 to 23
 4	     Minute	            0 to 59
-5	     Second	            0 to 61 (60/61 are leap seconds)
+5	     Second	            0 to 61 
 6	     Day of Week	    0 to 6 (Monday to Sunday)
-7	     Day of Year	    1 to 366 (Julian day)
+7	     Day of Year	    1 to 366
 8	     DST	            -1,0,1
 '''
 
